88c89c88bcc7223b11e3950a66d277400d62220e
usamakhalid <usamakhalid1600@gmail.com>
2023-02-07 17:25:01 +0300

    add Scroll logic to components and wrap scroll div around cardList to scroll over cards

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList';
-
+import Scroll from '../components/Scroll';
 class App extends React.Component{
 
   constructor(){
@@ -19,7 +19,11 @@ class App extends React.Component{
     <div className='tc'>
       <h1 className="f2">Crazy Bots!</h1>
       <SearchBox searchchange={this.handleChange} />
-      <CardList  />
+      <Scroll>
+        
+        <CardList  />
+
+      </Scroll>
     </div>
   )
 
