9a709977024007911aaa5911c43eb212e75ced20
usamakhalid <usamakhalid1600@gmail.com>
2023-02-07 12:30:51 +0300

    add tachyon style in cards to enhance viewport

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card=({id,name,email})=>{
     return(
         
-        <div>
+        <div className="grow bg-light-green dib br3 pa3 ma2 bw2 shadow-5">
             <img src={`https://robohash.org/${id}?200x200`} alt="Robot is Loading..." />
             <div className="tc">
               <h2>{name}</h2>
